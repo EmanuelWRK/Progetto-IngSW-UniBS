4cc02c477b46838ac9b7e8179abda05405b04117
beng002 <b.lupica@studenti.unibs.it>
2026-04-16 15:23:23 +0200

    Test

--- Elaborato_IngSW/src/it/unibs/ingsw/MainApp.java
+++ Elaborato_IngSW/src/it/unibs/ingsw/MainApp.java
@@ -3,8 +3,8 @@ package it.unibs.ingsw;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world+");
-
+		System.out.println("Hello world");
+		
 	}
 
 }
