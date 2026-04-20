712b39018775f196b5eb947a3b4e8f5631167563
emanuel <emanuel@macbookair.station>
2026-04-20 17:02:41 +0200

    Reingegnerizzazione, piccole modifiche

--- Elaborato_IngSW/src/it/unibs/ingsw/MainApp.java
+++ Elaborato_IngSW/src/it/unibs/ingsw/MainApp.java
@@ -2,11 +2,13 @@ package it.unibs.ingsw;
 
 import it.unibs.ingsw.view.AppView;
 import it.unibs.ingsw.model.AppModel;
+import it.unibs.ingsw.controller.AppController;
 
 public class MainApp {
 
 	private AppView view;
 	private AppModel model;
+	private AppController controller;
 	
 	public static void main(String[] args) {
 		MainApp app = new MainApp();
@@ -16,10 +18,12 @@ public class MainApp {
 	public MainApp() {
 		this.model = new AppModel();
 		this.view = new AppView();
+		this.controller = new AppController(this.model, this.view);
 	}
 
 	private void initialize() {
-		model.start();
-		view.start();
+		while(true) {
+			controller.start();
+		}
 	}
 }
---
+++ Elaborato_IngSW/src/it/unibs/ingsw/controller/AppController.java
@@ -0,0 +1,19 @@
+package it.unibs.ingsw.controller;
+
+import it.unibs.ingsw.model.AppModel;
+import it.unibs.ingsw.view.AppView;
+
+public class AppController {
+	AppModel model;
+	AppView view;
+	
+	public AppController(AppModel model, AppView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void start() {
+		
+	}
+	
+}
