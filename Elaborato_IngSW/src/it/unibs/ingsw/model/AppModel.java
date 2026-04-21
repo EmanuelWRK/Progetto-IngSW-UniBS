5facd2bc2e44a4190b91889c8bab303bc28ccc02
beng002 <b.lupica@studenti.unibs.it>
2026-04-21 18:32:02 +0200

    Piccole cose in Model

--- Elaborato_IngSW/src/it/unibs/ingsw/model/AppModel.java
+++ Elaborato_IngSW/src/it/unibs/ingsw/model/AppModel.java
@@ -1,7 +1,18 @@
 package it.unibs.ingsw.model;
 
 public class AppModel {
+	private String user = "admin";
+//	private String password = "password";
+	private boolean firstAccess = true;
+	
+	
 	public void start() {
+		User configuratore = new User();
 		
 	}
+
+	public String getUser() {
+		return user;
+	}
+	
 }
