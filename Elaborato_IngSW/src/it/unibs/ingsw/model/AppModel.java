b03f7d296ee76460b8786c6f1a7d17064ea0fe24
beng002 <b.lupica@studenti.unibs.it>
2026-05-02 15:18:27 +0200

    Implementato JSON

--- Elaborato_IngSW/src/it/unibs/ingsw/model/AppModel.java
+++ Elaborato_IngSW/src/it/unibs/ingsw/model/AppModel.java
@@ -1,6 +1,8 @@
 package it.unibs.ingsw.model;
 
 import it.unibs.ingsw.model.utenti.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 
 public class AppModel {
 	private String username = "admin";
@@ -15,8 +17,17 @@ public class AppModel {
 		switch (scelta) {
 		case 0: {
 			configuratore.setUsername();
+			
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+				// salva l'oggetto User su file JSON
+				mapper.writeValue(new File("user.json"), configuratore);
+				System.out.println("User salvato in JSON!");
+				} catch (Exception e) {
+					e.printStackTrace();
+					}
 			break;
-		}
+			}
 		case 1:{
 			break;
 		}
